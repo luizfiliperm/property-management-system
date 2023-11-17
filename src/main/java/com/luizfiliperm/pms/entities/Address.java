2c8e4361c068e57f1cea43bfbfb38f1616af5a5d
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-17 14:37:48 -0300

    Address entity

---
+++ src/main/java/com/luizfiliperm/pms/entities/Address.java
@@ -0,0 +1,31 @@
+package com.luizfiliperm.pms.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+}
