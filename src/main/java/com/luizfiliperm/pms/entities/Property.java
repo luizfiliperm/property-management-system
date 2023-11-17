5991fbd7d295fe92ba373924d2d5d1183be6d178
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-17 14:32:34 -0300

    Property Entity

---
+++ src/main/java/com/luizfiliperm/pms/entities/Property.java
@@ -0,0 +1,30 @@
+package com.luizfiliperm.pms.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contact")
+    private String contact;
+
+    @Column(name = "number_of_units")
+    private Integer numberOfUnits;
+
+    @Column(name = "description")
+    private String description;
+}
