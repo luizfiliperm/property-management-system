beafde0e35ddfc00c76ed43ecc58f2643d72d308
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-20 11:50:50 -0300

    Service layer of FindAll Properties

---
+++ src/main/java/com/luizfiliperm/pms/dtos/PageResponse.java
@@ -0,0 +1,19 @@
+package com.luizfiliperm.pms.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
--- src/main/java/com/luizfiliperm/pms/services/impl/PropertyServiceImpl.java
+++ src/main/java/com/luizfiliperm/pms/services/impl/PropertyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.luizfiliperm.pms.services.impl;
 
+import com.luizfiliperm.pms.dtos.PageResponse;
 import com.luizfiliperm.pms.dtos.PropertyDto;
 import com.luizfiliperm.pms.entities.Address;
 import com.luizfiliperm.pms.entities.Property;
@@ -7,7 +8,13 @@ import com.luizfiliperm.pms.repositories.AddressRepository;
 import com.luizfiliperm.pms.repositories.PropertyRepository;
 import com.luizfiliperm.pms.services.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PropertyServiceImpl implements PropertyService {
@@ -32,5 +39,20 @@ public class PropertyServiceImpl implements PropertyService {
     @Override
     public PropertyDto findById(Long id) {
         return new PropertyDto(propertyRepository.findById(id).orElseThrow());
+    }
+
+    @Override
+    public PageResponse<PropertyDto> findAll(int page, int size, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() :
+                Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(page,size,sort);
+
+        Page<Property> properties = propertyRepository.findAll(pageable);
+
+        List<Property> propertyList = properties.getContent();
+
+        List<PropertyDto> content = propertyList.stream().map(PropertyDto::new).toList();
+        return new PageResponse<PropertyDto>(content, properties.getNumber(), properties.getSize(), properties.getTotalElements(), properties.getTotalPages(), properties.isLast());
     }
 }
