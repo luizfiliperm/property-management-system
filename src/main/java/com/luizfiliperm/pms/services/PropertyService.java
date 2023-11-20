502a6a92b8c45864a0389147d06f7fdcfef2036a
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-20 11:50:50 -0300

    Service layer of FindAll Properties

--- src/main/java/com/luizfiliperm/pms/services/PropertyService.java
+++ src/main/java/com/luizfiliperm/pms/services/PropertyService.java
@@ -1,5 +1,6 @@
 package com.luizfiliperm.pms.services;
 
+import com.luizfiliperm.pms.dtos.PageResponse;
 import com.luizfiliperm.pms.dtos.PropertyDto;
 
 public interface PropertyService {
@@ -7,4 +8,6 @@ public interface PropertyService {
     PropertyDto register(PropertyDto propertyDto);
 
     PropertyDto findById(Long id);
+
+    PageResponse<PropertyDto> findAll(int page, int size, String sortBy, String sortDir);
 }
