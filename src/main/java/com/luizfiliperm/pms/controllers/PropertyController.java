e2a12a68c108235702e87a6a7bef6c8de14e4847
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-20 14:13:57 -0300

    FindAll properties paginated

--- src/main/java/com/luizfiliperm/pms/controllers/PropertyController.java
+++ src/main/java/com/luizfiliperm/pms/controllers/PropertyController.java
@@ -1,7 +1,9 @@
 package com.luizfiliperm.pms.controllers;
 
+import com.luizfiliperm.pms.dtos.PageResponse;
 import com.luizfiliperm.pms.dtos.PropertyDto;
 import com.luizfiliperm.pms.services.PropertyService;
+import com.luizfiliperm.pms.util.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,20 @@ public class PropertyController {
     PropertyService propertyService;
 
     @PostMapping
-    public ResponseEntity<PropertyDto> saveProperty(@RequestBody PropertyDto propertyDto){
+    public ResponseEntity<PropertyDto> saveProperty(@RequestBody PropertyDto propertyDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(propertyService.register(propertyDto));
     }
 
     @GetMapping("/{propertyId}")
-    public ResponseEntity<PropertyDto> findPropertyById(@PathVariable Long propertyId){
+    public ResponseEntity<PropertyDto> findPropertyById(@PathVariable Long propertyId) {
         return ResponseEntity.ok(propertyService.findById(propertyId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<PropertyDto>> findAll(@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+                                                             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+                                                             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+                                                             @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
+        return ResponseEntity.status(HttpStatus.OK).body(propertyService.findAll(pageNo, pageSize, sortBy, sortDir));
     }
 }
