feeb4b5bc59ee2dfd79c7d176be6c5cb0e284ea7
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-23 08:53:06 -0300

    Validating AddressDto

--- src/main/java/com/luizfiliperm/pms/dtos/property/PropertyDtoReceive.java
+++ src/main/java/com/luizfiliperm/pms/dtos/property/PropertyDtoReceive.java
@@ -2,7 +2,9 @@ package com.luizfiliperm.pms.dtos.property;
 
 import com.luizfiliperm.pms.dtos.AddressDto;
 import com.luizfiliperm.pms.entities.Property;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +25,8 @@ public class PropertyDtoReceive {
     @NotBlank(message = "The description is required")
     private String description;
 
+    @Valid
+    @NotNull(message = "The address is required")
     private AddressDto address;
 
     public PropertyDtoReceive(Property property){
