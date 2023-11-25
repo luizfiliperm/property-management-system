94819b60c484561d98a2e92e081da5f4fecf2acf
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-25 11:27:22 -0300

    Adding spring security dependency, and implementing UserDetails at User entity

--- src/main/java/com/luizfiliperm/pms/entities/user/User.java
+++ src/main/java/com/luizfiliperm/pms/entities/user/User.java
@@ -6,12 +6,18 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,4 +43,33 @@ public class User {
     @JoinColumn(name = "personal_information_id")
     private PersonalInformation personalInformation;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/luizfiliperm/pms/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.luizfiliperm.pms.repositories;
+
+import com.luizfiliperm.pms.entities.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
