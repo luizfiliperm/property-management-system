aecb63531d41e8c1a9b3e3803cc6810f87435cc6
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-27 09:37:29 -0300

    Implementing getSigningKey

--- src/main/java/com/luizfiliperm/pms/infra/security/services/impl/JwtServiceImpl.java
+++ src/main/java/com/luizfiliperm/pms/infra/security/services/impl/JwtServiceImpl.java
@@ -3,17 +3,25 @@ package com.luizfiliperm.pms.infra.security.services.impl;
 import com.luizfiliperm.pms.infra.security.services.JwtService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtServiceImpl implements JwtService {
 
-    @Value("${api.security.token.secret}")
-    private String key;
+    private final String SECRET_KEY;
 
-    public String getSigningKey() {
-        return key;
+    public JwtServiceImpl(@Value("${api.security.token.secret}") String secretKey) {
+        this.SECRET_KEY = secretKey;
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
     @Override
